9ad4484ae41d3accbcdb21b5b11f6b8be1ab6b56
Breno Freitas <brenofreitasmendes@gmail.com>
2025-02-27 22:33:59 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    devIndicators: false, // Desativa os indicadores de desenvolvimento
+};
 
 export default nextConfig;
